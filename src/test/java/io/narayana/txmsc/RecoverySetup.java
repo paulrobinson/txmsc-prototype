a3174b808dff2d4eac4b908e19478c50bd5fd83e
Paul Robinson <paul.robinson@redhat.com>
2013-09-02 16:07:38 +0100

    Register the RootTransactionRecoveryManager in the test code, rather than XML

--- src/test/java/io/narayana/txmsc/RecoverySetup.java
+++ src/test/java/io/narayana/txmsc/RecoverySetup.java
@@ -3,9 +3,12 @@ package io.narayana.txmsc;
 import com.arjuna.ats.arjuna.common.ObjectStoreEnvironmentBean;
 import com.arjuna.ats.arjuna.common.RecoveryEnvironmentBean;
 import com.arjuna.ats.arjuna.recovery.RecoveryManager;
+import com.arjuna.ats.arjuna.recovery.RecoveryModule;
 import com.arjuna.common.internal.util.propertyservice.BeanPopulator;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author paul.robinson@redhat.com 30/08/2013
@@ -18,6 +21,11 @@ public class RecoverySetup {
 
         RecoveryManager.delayRecoveryManagerThread();
         BeanPopulator.getDefaultInstance(RecoveryEnvironmentBean.class).setRecoveryBackoffPeriod(1);
+
+        List<RecoveryModule> recoveryModules = new ArrayList<RecoveryModule>();
+        recoveryModules.add(new RootTransactionRecoveryModule());
+        BeanPopulator.getDefaultInstance(RecoveryEnvironmentBean.class).setRecoveryModules(recoveryModules);
+
         recoveryManager = RecoveryManager.manager();
     }
 
